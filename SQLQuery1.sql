f73bc5ee0f105cf3b7f84f737d09845a03cd7e7f
naklida172 <105726177+naklida172@users.noreply.github.com>
2024-06-08 20:27:50 +0800

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,1 +1,43 @@
-﻿Create table Menu_Item
+Create table [User]
+([UserID] nvarchar(50) NOT NULL PRIMARY KEY,
+[Name] nvarchar(50),
+[Username] nvarchar(20),
+[Password] nvarchar(20),
+[Role] nvarchar(10),
+[Email] nvarchar(50),
+[Phone] nvarchar(20),
+[Address] nvarchar(100)
+);
+
+--Drop table [User];
+
+INSERT INTO [User] VALUES 
+('UN00','Test','Login','Password','manger', 'testE','testP','testA'),
+('UN01', 'John Doe', 'john_admin', 'admin123', 'admin', 'john.doe@example.com', '+1 123-456-7890', '123 Main St, Anytown, USA'),
+('UN02','Jane Smith', 'janemanag', 'manager456', 'manager', 'jane.smith@example.com', '+1 987-654-3210', '456 Elm Ave, Cityville, USA'),
+('UN03', 'Michael Johnson', 'chef_mike', 'secretchef', 'chef', 'mike.johnson@example.com', '+1 555-123-4567', '789 Oak Rd, Culinarytown, USA'),
+('UN04', 'Emily Brown', 'emily_customer', 'customer789', 'customer', 'emily.brown@example.com', '+1 234-567-8901', '101 Maple Ln, Shopper City, USA')
+;
+
+Create table Menu_item
+(ItemID int NOT NULL PRIMARY KEY,
+[Name] nvarchar(50),
+Price float,
+[Description] nvarchar(200),
+Category nvarchar(10),
+Callories int,
+Ingridients nvarchar(300)
+);
+--Drop Table Menu_item;
+INSERT INTO Menu_item (ItemID, [Name], Price, [Description], Category, Callories, Ingridients)
+VALUES
+    (201, 'Napkins', 1.20, 'Disposable napkins for cleaning hands and face', 'Supplies', NULL, NULL),
+    (202, 'Plastic Cups', 2.00, 'Disposable plastic cups for beverages', 'Supplies', NULL, NULL),
+    (203, 'Straws', NULL, 'Plastic straws for drinks', 'Supplies', NULL, NULL),
+    (204, 'Stir Sticks', NULL, 'Wooden stir sticks for coffee and tea', 'Supplies', NULL, NULL),
+    (205, 'Takeout Containers', 3.50, 'Disposable takeout containers for food', 'Supplies', NULL, NULL),
+    (101, 'Cheeseburger', 8.99, 'Classic cheeseburger with beef patty and cheddar cheese', 'Burgers', 550, 'Chicken patty, cheddar cheese, lettuce, tomato, onion, pickles'),
+    (102, 'Margherita Pizza', 12.49, 'Traditional pizza with tomato sauce, mozzarella, and basil', 'Pizza', 750, 'Pizza dough, tomato sauce, mozzarella cheese, fresh basil'),
+    (103, 'Caesar Salad', 6.99, 'Classic Caesar salad with romaine lettuce and croutons', 'Salads', 350, 'Romaine lettuce, croutons, Caesar dressing, Parmesan cheese')
+;
+Select Ingridients from Menu_item where ItemID = 203;
